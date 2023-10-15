f1698d0308e2b96c855445f60a2c2cb903af24d1
Sandesh Tidake0408 <sandeshtidake37@gmail.com>
2023-10-15 19:36:17 +0530

    adding trigger

---
+++ trigger.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE TRIGGER BOOKSTORE AFTER
+    INSERT OR UPDATE OR DELETE ON LIBRARY FOR EACH ROW ENABLE
+BEGIN
+    IF UPDATING THEN
+        DBMS_OUTPUT.PUT_LINE(:OLD.STATUS);
+        INSERT INTO LIBRARY_AUDIT VALUES(
+            SYSDATE,
+            :OLD.BOOK_NAME,
+            :OLD.STATUS,
+            :NEW.STATUS,
+            'Update'
+        );
+    ELSIF INSERTING THEN
+        DBMS_OUTPUT.PUT_LINE(:NEW.BOOK_NAME
+                                  || ' Inserting');
+        INSERT INTO LIBRARY_AUDIT VALUES(
+            SYSDATE,
+            :NEW.BOOK_NAME,
+            NULL,
+            :NEW.STATUS,
+            'Insert'
+        );
+    ELSIF DELETING THEN
+        DBMS_OUTPUT.PUT_LINE(:OLD.BOOK_NAME
+                                  || ' Deleting');
+        INSERT INTO LIBRARY_AUDIT VALUES(
+            SYSDATE,
+            :OLD.BOOK_NAME,
+            :OLD.STATUS,
+            NULL,
+            'Delete'
+        );
+    END IF;
+END;
+/
